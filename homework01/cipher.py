922ea0030967f544c560d19bae380ac76c998d9e
Артём Заборщиков <a1az1z@ya.ru>
2024-09-30 04:36:10 +0300

    Add changes for PEP8 rules complete

--- homework01/cipher.py
+++ homework01/cipher.py
@@ -9,11 +9,11 @@ SIZE_OF_ALPHABET = Z_ORD - A_ORD + 1
 
 def get_start_ord(char_ord: int) -> Optional[int]:
     '''
-    Return ord of 'A' in the alphabet (lower or capital) and None 
+    Return ord of 'A' in the alphabet (lower or capital) and None
     if input is not an ord of letter of English alphabet.
-    
+
     in  (int):
-        letter ord 
+        letter ord
     out (int):
         starter ord or None (if it is not a letter)
     '''
@@ -22,19 +22,20 @@ def get_start_ord(char_ord: int) -> Optional[int]:
         return A_ORD
     elif A_ORD_CAP <= char_ord <= Z_ORD_CAP:
         return A_ORD_CAP
-    
+
     return None
-      
+
 
 def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
     '''
-    Apply a Caesar cipher to text (only for english letters, other will be ignored)
-    
+    Apply a Caesar cipher to text
+    (only for english letters, other will be ignored)
+
     in:
         plaintext   (str): your text for encrypting
         *shift      (int): shift for encrypting, default 3
-    out (str): 
-        encrypted text 
+    out (str):
+        encrypted text
 
     >>> encrypt_caesar("PYTHON")
     'SBWKRQ'
@@ -46,31 +47,33 @@ def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
     ''
     '''
 
-    raw_ciphertext = [] 
+    raw_ciphertext = []
 
     for char in plaintext:
         char_ord = ord(char)
         start = get_start_ord(char_ord)
 
         if start is not None:
-            encrypted_alpha = chr(start + (char_ord - start + shift) % SIZE_OF_ALPHABET)
+            encrypted_alpha = chr(
+                start + (char_ord - start + shift) % SIZE_OF_ALPHABET)
         else:
             encrypted_alpha = char
-        
+
         raw_ciphertext.append(encrypted_alpha)
-                
+
     return ''.join(raw_ciphertext)
 
 
 def decrypt_caesar(ciphertext: str, shift: int = 3) -> str:
     '''
-    Decrypt a Caesar cipher to text (only for english letters, other will be ignored)
-    
+    Decrypt a Caesar cipher to text
+    (only for english letters, other will be ignored)
+
     in:
         plaintext   (str): your text for decrypting
         *shift      (int): shift for decrypting, default 3
-    out (str): 
-        decrypted text 
+    out (str):
+        decrypted text
 
     >>> decrypt_caesar("SBWKRQ")
     'PYTHON'
@@ -82,7 +85,8 @@ def decrypt_caesar(ciphertext: str, shift: int = 3) -> str:
     ''
     '''
 
-    return encrypt_caesar(ciphertext, shift= -shift)
+    return encrypt_caesar(ciphertext, shift=-shift)
+
 
 def decrypt_key(key: str) -> list[int]:
     '''
@@ -101,19 +105,21 @@ def decrypt_key(key: str) -> list[int]:
             int_key.append(char_ord - start)
         else:
             raise ValueError(f'Char {char} is not English letter')
-        
+
     return tuple(int_key)
 
-def encrypt_vigenere(plaintext: str, key: str, decrypt: bool=False) -> str:
+
+def encrypt_vigenere(plaintext: str, key: str, decrypt: bool = False) -> str:
     '''
-    Apply a Vigenere cipher to text (only for english letters, other will be ignored)
-    
+    Apply a Vigenere cipher to text
+    (only for english letters, other will be ignored)
+
     in:
         plaintext   (str):  your text for encrypting
         key         (int):  key for encrypting
         *decrypt    (bool): decrypt plaintext by key, defalut False
-    out (str): 
-        encrypted text 
+    out (str):
+        encrypted text
 
     >>> encrypt_vigenere("PYTHON", "A")
     'PYTHON'
@@ -137,24 +143,27 @@ def encrypt_vigenere(plaintext: str, key: str, decrypt: bool=False) -> str:
 
         if start is not None:
             shift = int_key[(i - index_shift) % key_size]
-            encrypted_alpha = chr(start + (char_ord - start + shift) % SIZE_OF_ALPHABET)
+            encrypted_alpha = chr(
+                start + (char_ord - start + shift) % SIZE_OF_ALPHABET)
         else:
             encrypted_alpha = char
-        
+
         raw_ciphertext.append(encrypted_alpha)
 
     return ''.join(raw_ciphertext)
 
+
 def decrypt_vigenere(plaintext: str, key: str) -> str:
     '''
-    Decrypt a Vigenre cipher to text (only for english letters, other will be ignored)
-    
+    Decrypt a Vigenre cipher to text
+    (only for english letters, other will be ignored)
+
     in:
         plaintext   (str): your text for decrypting
         key         (str): key for decrypting
-    out (str): 
-        decrypted text 
-    
+    out (str):
+        decrypted text
+
     >>> decrypt_vigenere("PYTHON", "A")
     'PYTHON'
     >>> decrypt_vigenere("python", "a")
@@ -165,11 +174,14 @@ def decrypt_vigenere(plaintext: str, key: str) -> str:
 
     return encrypt_vigenere(plaintext, key, True)
 
+
 if __name__ == '__main__':
     # Ceaser
     print('CEASER')
-    plain_texts = ['PYTHON', 'python', 'Python3.6', '', 'abc', 'zaz', 'Prodam garazh']
-    cipher_texts = ['SBWKRQ', 'sbwkrq', 'Sbwkrq3.6', '', 'def', 'cdc', 'Surgdp jdudck']
+    plain_texts = ['PYTHON', 'python', 'Python3.6',
+                   '', 'abc', 'zaz', 'Prodam garazh']
+    cipher_texts = ['SBWKRQ', 'sbwkrq', 'Sbwkrq3.6',
+                    '', 'def', 'cdc', 'Surgdp jdudck']
 
     errors_encrypt = 0
     errors_decrypt = 0
@@ -185,15 +197,19 @@ if __name__ == '__main__':
             print(decrypted, plain_texts[i])
             errors_decrypt += 1
 
-    print(f'Passed {len(plain_texts) - errors_encrypt}/{len(plain_texts)} tests in encrypt')
-    print(f'Passed {len(plain_texts) - errors_decrypt}/{len(plain_texts)} tests in decrypt')
+    encrypt_score = f'{len(plain_texts) - errors_encrypt}/{len(plain_texts)}'
+    decrypt_score = f'{len(plain_texts) - errors_decrypt}/{len(plain_texts)}'
 
+    print(f'Passed {encrypt_score} tests in encrypt')
+    print(f'Passed {decrypt_score} tests in decrypt')
 
     # Vigenere
     print('\nVIGENERE')
-    plain_texts = ['PYTHON', 'python', 'ATTACKATDAWN', 'garazhvsyoechoprodaetsyaestakcii']
+    plain_texts = ['PYTHON', 'python', 'ATTACKATDAWN',
+                   'garazhvsyoechoprodaetsyaestakcii']
     keys = ['A', 'a', 'LEMON', 'nedorogo']
-    cipher_texts = ['PYTHON', 'python', 'LXFOPVEFRNHR', 'teuoqvbglshqycvfbhdskgeorwwobqow']
+    cipher_texts = ['PYTHON', 'python', 'LXFOPVEFRNHR',
+                    'teuoqvbglshqycvfbhdskgeorwwobqow']
 
     errors_encrypt = 0
     errors_decrypt = 0
@@ -209,5 +225,8 @@ if __name__ == '__main__':
             print(decrypted, plain_texts[i])
             errors_decrypt += 1
 
-    print(f'Passed {len(plain_texts) - errors_encrypt}/{len(plain_texts)} tests in encrypt')
-    print(f'Passed {len(plain_texts) - errors_decrypt}/{len(plain_texts)} tests in decrypt')
+    encrypt_score = f'{len(plain_texts) - errors_encrypt}/{len(plain_texts)}'
+    decrypt_score = f'{len(plain_texts) - errors_decrypt}/{len(plain_texts)}'
+
+    print(f'Passed {encrypt_score} tests in encrypt')
+    print(f'Passed {decrypt_score} tests in decrypt')
