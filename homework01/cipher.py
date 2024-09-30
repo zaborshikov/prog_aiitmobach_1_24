8b74ed0e9f747c9fce5eddb1fe99c81bf2adda1e
Артём Заборщиков <a1az1z@ya.ru>
2024-09-30 03:29:26 +0300

    Add Vigenere cipher

--- homework01/cipher.py
+++ homework01/cipher.py
@@ -84,9 +84,90 @@ def decrypt_caesar(ciphertext: str, shift: int = 3) -> str:
 
     return encrypt_caesar(ciphertext, shift= -shift)
 
+def decrypt_key(key: str) -> list[int]:
+    '''
+    Create key from word for Vigenere ciphere
+
+    in (str): str key (only english letters)
+    out (list[int]): key for encrypt/decrypt
+    '''
+    int_key = []
+
+    for char in key:
+        char_ord = ord(char)
+        start = get_start_ord(char_ord)
+
+        if start is not None:
+            int_key.append(char_ord - start)
+        else:
+            raise ValueError(f'Char {char} is not English letter')
+        
+    return tuple(int_key)
+
+def encrypt_vigenere(plaintext: str, key: str, decrypt=False) -> str:
+    '''
+    Apply a Vigenere cipher to text (only for english letters, other will be ignored)
+    
+    in:
+        plaintext   (str):  your text for encrypting
+        key         (int):  key for encrypting
+        *decrypt    (bool): decrypt plaintext by key, defalut False
+    out (str): 
+        encrypted text 
+
+    >>> encrypt_vigenere("PYTHON", "A")
+    'PYTHON'
+    >>> encrypt_vigenere("python", "a")
+    'python'
+    >>> encrypt_vigenere("ATTACKATDAWN", "LEMON")
+    'LXFOPVEFRNHR'
+    '''
+
+    raw_ciphertext = []
+    int_key = decrypt_key(key)
+    key_size = len(int_key)
+
+    if decrypt:
+        int_key = [-i for i in int_key]
+
+    for i, char in enumerate(plaintext):
+        char_ord = ord(char)
+        start = get_start_ord(char_ord)
+        shift = int_key[i % key_size]
+
+        if start is not None:
+            encrypted_alpha = chr(start + (char_ord - start + shift) % SIZE_OF_ALPHABET)
+        else:
+            encrypted_alpha = char
+        
+        raw_ciphertext.append(encrypted_alpha)
+                
+    return ''.join(raw_ciphertext)
+
+def decrypt_vigenere(plaintext: str, key: str) -> str:
+    '''
+    Decrypt a Vigenre cipher to text (only for english letters, other will be ignored)
+    
+    in:
+        plaintext   (str): your text for decrypting
+        key         (str): key for decrypting
+    out (str): 
+        decrypted text 
+    
+    >>> decrypt_vigenere("PYTHON", "A")
+    'PYTHON'
+    >>> decrypt_vigenere("python", "a")
+    'python'
+    >>> decrypt_vigenere("LXFOPVEFRNHR", "LEMON")
+    'ATTACKATDAWN'
+    '''
+
+    return encrypt_vigenere(plaintext, key, True)
 
 if __name__ == '__main__':
-    shift = 3
+    
+    # Ceaser
+    print('CEASER')
     plain_texts = ['PYTHON', 'python', 'Python3.6', '', 'abc', 'zaz', 'Prodam garazh']
     cipher_texts = ['SBWKRQ', 'sbwkrq', 'Sbwkrq3.6', '', 'def', 'cdc', 'Surgdp jdudck']
 
